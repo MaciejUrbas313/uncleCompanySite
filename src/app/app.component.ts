2940007ce4ae7178c7c88410133be5a8226a7b06
Maciej Urbas <maciejurbas313@gmail.com>
2023-11-24 17:38:22 +0100

    :sparkles: Change values from strict vw to clamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { AboutComponent } from './about/about.component';
 import { NavComponent } from "./nav/nav.component";
 
 @Component({
@@ -8,7 +10,7 @@ import { NavComponent } from "./nav/nav.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [CommonModule, RouterOutlet, NavComponent]
+    imports: [CommonModule, RouterOutlet, AboutComponent, NavComponent]
 })
 export class AppComponent {
   title = 'uncleCompanySite';
