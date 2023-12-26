f34e7d815f3089410b77a308dd61540e6a2283c4
Maciej Urbas <maciejurbas313@gmail.com>
2023-12-26 19:42:50 +0100

    :zap: Set main color from `#7f9fbe` to `#398EE0`
    Change fill color foreach SVG object;
    
    Add `12.jpg` picture, change order for `swiper-slide` arts
    and change last one for this already added;
    
    Change CSS attribute for `.contact-rectangle` class
    from `display: block;` to `display: none`.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,5 +9,29 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  scrollToContact() {
+    let scrollHeight: number = 0;
+    let heightHelper: any = 0;
 
+    heightHelper = document.querySelector('.nav')?.clientHeight;
+    if (heightHelper != undefined) {
+      scrollHeight = heightHelper;
+    }
+    heightHelper = document.querySelector('.about')?.clientHeight;
+    if (heightHelper != undefined) {
+      scrollHeight = scrollHeight + heightHelper;
+    }
+    heightHelper = document.querySelector('.offer')?.clientHeight;
+    if (heightHelper != undefined) {
+      scrollHeight = scrollHeight + heightHelper;
+    }
+
+    setTimeout(() => {
+      window.scroll({
+        top: scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }, 100);
+  }
 }
